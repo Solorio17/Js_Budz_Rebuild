5da36564bd407219fb417b8db5b76dfeae40692a
Jason Solorio <jasonsoloriom@gmail.com>
2019-03-25 19:29:10 -0600

    deleteBud is only taking in the id so it doesn't need much configuration, only passing in the id. updateBud needed a little more, which was to take all arguments, delete the existing id, then pass the args as data and specify where which is the id

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,6 +3,21 @@ const Mutation = {
         return context.prisma.createBudType({
             ...args
         })
+    },
+    deleteBud: (root, args, context, info) => {
+        return context.prisma.deleteBudType({
+            ...args
+        })
+    },
+    updateBud: (root, args, context, info) => {
+        const updates = {...args};
+        delete updates.id;
+        return context.prisma.updateBudType({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info)
     }
 };
     
