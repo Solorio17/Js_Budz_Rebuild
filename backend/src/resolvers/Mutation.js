da9d2329280e9eeefcaf5d21403bbd8468a68a4d
Jason Solorio <jasonsoloriom@gmail.com>
2019-03-29 14:28:25 -0600

    Updated Resolvers to use the db and not the prisma to access the methods. And updated the ways the data is being passed and returned

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,18 +1,20 @@
 const Mutation = {
     createBud: (root, args, context, info) => {
-        return context.prisma.createBudType({
-            ...args
+        const budData = {...args};
+
+        return context.db.mutation.createBudType({
+            data: budData
         })
     },
     deleteBud: (root, args, context, info) => {
-        return context.prisma.deleteBudType({
-            ...args
+        return context.db.mutation.deleteBudType({
+            where: {id: args.where.id}
         })
     },
     updateBud: (root, args, context, info) => {
         const updates = {...args};
         delete updates.id;
-        return context.prisma.updateBudType({
+        return context.db.mutation.updateBudType({
             data: updates,
             where: {
                 id: args.id
