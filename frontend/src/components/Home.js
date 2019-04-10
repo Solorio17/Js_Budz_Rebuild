090754328afd3feeaa6d7f454e03dd9cd877b309
Jason Solorio <jasonsoloriom@gmail.com>
2019-04-09 23:09:10 -0600

    Nothing much yet, just a button that routes to all budz

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/styles.css'
+
+class Home extends Component{
+  render(){
+    return(
+      <div className="mainHomeContainer">
+        <a href="/budz">Budz</a>
+      </div>
+    )
+  }
+}
+
+export default Home;
